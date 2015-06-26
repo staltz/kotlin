ede40be7b6cca7ad4cb6e3bf3a0c25e5f5f30466
Alexander Udalov <Alexander.Udalov@jetbrains.com>
2015-06-26 21:25:20 +0300

    Make CLASS default class kind in binary metadata
    Don't write the kind to the class file if it's CLASS to save some bytes

--- compiler/backend/src/org/jetbrains/kotlin/codegen/ImplementationBodyCodegen.java
+++ compiler/backend/src/org/jetbrains/kotlin/codegen/ImplementationBodyCodegen.java
@@ -239,7 +239,8 @@ public class ImplementationBodyCodegen extends ClassBodyCodegen {
             kind = KotlinClass.Kind.ANONYMOUS_OBJECT;
         }
         else if (isTopLevelOrInnerClass(descriptor)) {
-            kind = KotlinClass.Kind.CLASS;
+            // Default value is Kind.CLASS
+            kind = null;
         }
         else {
             // LOCAL_CLASS is also written to inner classes of local classes
@@ -257,11 +258,13 @@ public class ImplementationBodyCodegen extends ClassBodyCodegen {
 
         AnnotationVisitor av = v.getVisitor().visitAnnotation(asmDescByFqNameWithoutInnerClasses(JvmAnnotationNames.KOTLIN_CLASS), true);
         av.visit(JvmAnnotationNames.ABI_VERSION_FIELD_NAME, JvmAbi.VERSION);
-        av.visitEnum(
-                JvmAnnotationNames.KIND_FIELD_NAME,
-                Type.getObjectType(KotlinClass.KIND_INTERNAL_NAME).getDescriptor(),
-                kind.toString()
-        );
+        if (kind != null) {
+            av.visitEnum(
+                    JvmAnnotationNames.KIND_FIELD_NAME,
+                    Type.getObjectType(KotlinClass.KIND_INTERNAL_NAME).getDescriptor(),
+                    kind.toString()
+            );
+        }
         AnnotationVisitor array = av.visitArray(JvmAnnotationNames.DATA_FIELD_NAME);
         for (String string : BitEncoding.encodeBytes(SerializationUtil.serializeClassData(data))) {
             array.visit(null, string);
